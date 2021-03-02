e3972c9bb42be0257c6f9601687104239a5201e2
Rodney <rodpereira@gmail.com>
2021-03-02 09:18:32 -0300

    ...

---
+++ ambito.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var global = 'variable global';
+
+function MostrarConsola(){
+    var variable_funcion = 'variable dentro de la funcion';
+    console.log(global);
+    console.log(variable_funcion);
+}
+MostrarConsola();
