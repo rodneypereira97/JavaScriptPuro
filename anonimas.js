da5304f307f850f6b912b5e3cadfe7eb5248ee05
Rodney <rodpereira@gmail.com>
2021-03-02 08:52:38 -0300

    cambios 2.0

---
+++ anonimas.js
@@ -0,0 +1,14 @@
+'use strict';
+
+//Funciones anonimas.
+
+function sumar(n1,n2,multiplicar) {
+    var suma = n1 + n2;
+    multiplicar(suma);
+    return suma;
+};
+
+sumar(5,7,function(d){
+    console.log('La suma es '+ d);
+    console.log('La multiplicacion es ' +d * 2);
+})
